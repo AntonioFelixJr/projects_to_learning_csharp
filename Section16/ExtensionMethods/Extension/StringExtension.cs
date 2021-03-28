e333653cb78db059261a509f3362a311ea5cf925
Antonio Felix <antonio.felixjr@hotmail.com>
2021-03-28 01:33:39 -0300

    Section 16

---
+++ Section16/ExtensionMethods/Extension/StringExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace System
+{
+    static class StringExtension
+    {
+        public static string Cut(this String thisObj, int limit)
+        {
+            if (thisObj.Length <= limit)
+                return thisObj.ToString();
+
+            return thisObj.Substring(0, limit).TrimEnd() + "...";
+        }
+    }
+}
---
+++ Section16/ExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 3, 28, 0, 0, 0);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string str = "Legal esse lance de Extension Methods";
+            Console.WriteLine(str.Cut(25));
+        }
+    }
+}
